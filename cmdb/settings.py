2fc011ca00f5a50ae0f54eb357d88a4bac78a4d9
qsm365 <qsm365@gmail.com>
2015-11-30 00:11:22 -0800

    use a base_url variable as context path

--- cmdb/settings.py
+++ cmdb/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.core.context_processors.static',
+                'cmdb.context_processors.context',
             ],
         },
     },
